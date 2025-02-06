75ab6d089ef8b12afdf4f32f46ed45cbda39a35f
AkhatorEnosa <aosakhogba@gmail.com>
2025-02-06 22:00:15 +0100

    Site name change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="w-full flex justify-center items-center mb-5 lg:mb-10 sticky top-0 bg-black/20 backdrop-blur-md">
       <a href="/" className="w-full flex justify-center items-center gap-2">
         <img src="/logo.png" alt="logo" className="size-10"/>
-        <h2>MOVIEHUB</h2>
+        <h2>MOOVEES</h2>
       </a>
     </nav>
   )

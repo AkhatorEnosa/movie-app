255e50bca888ad3e2a8b77fcc5ac591dfa88ae14
AkhatorEnosa <aosakhogba@gmail.com>
2025-02-06 22:19:50 +0100

    Nav bar z-index value

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <nav className="w-full flex justify-center items-center mb-5 py-4 lg:mb-10 sticky top-0 bg-black/20 backdrop-blur-sm">
+    <nav className="w-full flex justify-center items-center mb-5 py-4 lg:mb-10 sticky top-0 bg-black/20 backdrop-blur-sm z-50">
       <a href="/" className="w-full flex justify-center items-center gap-2">
         <img src="/logo.png" alt="logo" className="size-10"/>
         <h2>MOOVEES</h2>
